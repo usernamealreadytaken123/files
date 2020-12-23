397869734b3e1c4c036db9333fcddb9dcda25c96
usernamealreadytaken123 <73428467+usernamealreadytaken123@users.noreply.github.com>
2020-12-23 23:19:44 +0700

    Update attack.c

--- attack.c
+++ attack.c
@@ -11,15 +11,15 @@ using namespace std;
 void code(char str[], int str1[])
 {
 	int i = 0, j = 0;
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i=i+1)
 	{
 		str1[i] = (int)str[i];
-		j++;
+		j=j+1;
 	}
-	cout << "Encrypted string: ";
-	for (i = 0; i < j; i++)
+	printf ("Encrypted string: ");
+	for (i = 0; i < j; i=i+1)
 	{
-		cout << str1[i] << ' ';
+		printf(" %d",  str1[i]);
 	}
 	cout << endl;
 
@@ -28,24 +28,29 @@ void cipher(int str1[], int str4[], int d, int f,int a,int b)
 {
 	int j = 0;
 	int c = 0;
-	for (int a = 1; a < 25; a = a + 2) 
-		for (int b = 0; b < 5; b = b + 1){
-			for (int i = 0; i < d ; i++)
+	for (int a = 1; a < 26; a = a + 2) 
+		for (int b = 0; b < 26; b = b + 1){
+			for (int i = 0; i <= d ; i=i+1)
 			{
-				for (int j = 0; j < f; j++)
-				{
-					if (str1[i] == (((a * (str4[j]-97) + b) % 26) + 97))
+					if (str4[i] == (((a * (str1[i]-97) + b) % 26) + 97))
 					{
-						c++;
-					}
+						c=c+1;
 				
+					}
+					if (c == d)
+					{
+						printf("a=%d\n", a);
+						printf("a=%d\n", b);;
+						goto stop;
+					}
 				}
-
+			c = 0;
 			}
-		if (c == d) goto stop;
-	}
+		
+	
 stop:
 	a = a;
+	
 }
 
 		
@@ -56,18 +61,16 @@ int main()
 {
 	char str[256], str3[256], str5[256];
 	int str1[256], c, str4[256], str6[256];
-	int a = 1;
-	int b = 0;
-	cout << "enter string: ";
+	int a=1;
+	int b=0;
+	printf ("enter string: ");
 	cin.getline(str, 256);
 	int d = strlen(str);
 	code(str, str1);
-	cout << "key=";
+	printf ("key=");
 	cin.getline(str3, 256);
 	int f = strlen(str3);
 	code(str3, str4);
 	cipher(str1, str4, d, f,a,b);
-	cout << a;
-	cout << b;
 	return 0;
 }
