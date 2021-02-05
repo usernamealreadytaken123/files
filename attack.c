a4e1bc2a55a71d3fde1c788c7fa2e744107565ec
usernamealreadytaken123 <73428467+usernamealreadytaken123@users.noreply.github.com>
2021-02-05 22:40:25 +0700

    Update attack.c

--- attack.c
+++ attack.c
@@ -1,13 +1,11 @@
-#include <iostream>
+#include <stdio.h>
 #include <conio.h>
-#include <cstring>
+#include <string.h>
 #include <stdlib.h>
-
-using namespace std;
-
 
 
 void code(char str[], int str1[])
+
 {
 	int i = 0, j = 0;
 	for (i = 0; str[i] != '\0'; i=i+1)
@@ -20,15 +18,15 @@ void code(char str[], int str1[])
 	{
 		printf(" %d",  str1[i]);
 	}
-	cout << endl;
+	printf(" \n");
 
 }
-void cipher(int str1[], int str4[], int d, int f,int a,int b)
+void cipher( int str1[], int str4[], int d, int f,int a,int b, int* e, int* g, int* h )
 {
 	int j = 0;
 	int c = 0;
 	for (int a = 1; a < 26; a = a + 2) 
-		for (int b = 0; b < 26; b = b + 1){
+		for (int b = 0; b < 25; b = b + 1){
 			for (int i = 0; i <= d ; i=i+1)
 			{
 					if (str4[i] == (((a * (str1[i]-97) + b) % 26) + 97))
@@ -38,21 +36,24 @@ void cipher(int str1[], int str4[], int d, int f,int a,int b)
 					}
 					if (c == d)
 					{
-						printf("a=%d\n", a);
-						printf("a=%d\n", b);
+				
+						*e = a;
+						*g = b;
 						goto stop;
 					}
 				}
 			c = 0;
 			}
-		
 	
+
 stop:
-	a = a;
+	a=a;
 	
+
 }
 
 		
+
 
 
 
@@ -62,14 +63,28 @@ int main()
 	int str1[256], c, str4[256], str6[256];
 	int a=1;
 	int b=0;
+	int e, g;
+	int h;
 	printf ("enter string: ");
-	cin.getline(str, 256);
+	gets(str);
 	int d = strlen(str);
 	code(str, str1);
 	printf ("key=");
-	cin.getline(str3, 256);
+	gets(str3);
 	int f = strlen(str3);
+	if (f != d) 
+		goto error;
+	
 	code(str3, str4);
-	cipher(str1, str4, d, f,a,b);
+	cipher(str1, str4, d, f,a,b,&e,&g,&h);
+	if (e<0)
+		goto error;
+	printf("a=%d\n", e);
+	printf("b=%d\n", g);
+	goto exit;
+error:
+	
+		printf("error");
+		exit:
 	return 0;
 }
